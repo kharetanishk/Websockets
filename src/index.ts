e55549f7b1d802c016cbe6a0b4bfe48adaeb2d38
Tanishk khare <kharetanishk18@gmail.com>
2025-07-21 13:52:47 +0530

    Simplet chat between server and the user

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+//intialising the websocket server
+import { WebSocketServer } from "ws";
+
+let userCount = 0;
+const tanuwss = new WebSocketServer({ port: 1600 });
+
+//connection of a server
+tanuwss.on("connection", (socket) => {
+  userCount++;
+  console.log("Connection estalished" + " " + "#" + userCount);
+  socket.send("Hello world");
+
+  socket.on("message", (msg) => {
+    console.log(`User messaage : ${msg.toString()}`); //this is the user message
+    setTimeout(() => {
+      socket.send(
+        ` Server Message : Okay i have recieved your message is this was your msg -> ${msg.toString()}`
+      );
+    }, 4000);
+
+    console.log("Simple chat between user and server");
+  });
+});

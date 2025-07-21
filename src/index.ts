3e6d7da768247956e5ea5a39d8d13432ec831ac9
Tanishk khare <kharetanishk18@gmail.com>
2025-07-21 14:12:31 +0530

    Broadcasting message

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,24 @@ import { WebSocketServer } from "ws";
 
 let userCount = 0;
 const tanuwss = new WebSocketServer({ port: 1600 });
+let allSockets = [];
 
 //connection of a server
 tanuwss.on("connection", (socket) => {
+  allSockets.push(socket);
   userCount++;
   console.log("Connection estalished" + " " + "#" + userCount);
-  socket.send("Hello world");
 
   socket.on("message", (msg) => {
     console.log(`User messaage : ${msg.toString()}`); //this is the user message
-    setTimeout(() => {
-      socket.send(
-        ` Server Message : Okay i have recieved your message is this was your msg -> ${msg.toString()}`
-      );
-    }, 4000);
 
-    console.log("Simple chat between user and server");
+    //but now the socket wants to send the response that can appear in
+    // everyones server who is conneccted within it for that
+
+    //broadcasting
+    allSockets.forEach((e) => e.send("User message" + " " + msg.toString()));
+    allSockets.forEach((e) =>
+      e.send("Server message : HEllo world form the server")
+    );
   });
 });
